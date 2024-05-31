6255d329090f72b14463c621bd8d189336e64690
Hyeon-hak <hyeonhak.kim.dev@gmail.com>
2024-05-31 18:57:17 +0900

    ♻️  Refactor : Remove redundant print statements

--- c-binary/hello.c
+++ c-binary/hello.c
@@ -3,9 +3,5 @@
 int main(int argc, char *argv[])
 {
     printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
     return 0;
 }
